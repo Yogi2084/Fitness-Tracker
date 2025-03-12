2f41660d5ba4d952a6fdaea0515bbd346662de25
Yogi2084 <1si22is406@sit.ac.in>
2025-03-12 15:51:57 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {
     addUser,logWorkout,getAllWorkoutsOf,getAllWorkoutsByType,getUsers,getUser,updateUser } from './tracker';
 
 addUser("U001","yogI",24,59,5.2);
-addUser("U0022","manu",20,70,5.7);
+addUser("U002","manu",20,70,5.7);
 addUser("U003","raghu",30,45,6.1);
 
 logWorkout("U001", { type: "running", duration: 30, caloriesBurned: 300, date: new Date() });
@@ -22,4 +22,4 @@ console.log(getAllWorkoutsOf("U003"));
 console.log(getAllWorkoutsByType("U003", "gym")); 
 
 updateUser("U001", { weight: 64, height: 169 });
-console.log(getUser("US001"));
+console.log(getUser("U001"));
